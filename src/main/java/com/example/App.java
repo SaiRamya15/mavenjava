0435645640ddeb41a75c8cd5c96a9fb372e7265d
SaiRamya15 <sairamya5169@gmail.com>
2025-05-17 12:38:56 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -10,15 +10,22 @@ public class App {
     }
 }*/
 
-import java.util.logging.Logger;
 
-class App {
+package com.example;
 
-  Logger logger = Logger.getLogger(getClass().getName());
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-  public void doSomething() {
-    // ...
-    logger.info("My Message");  // Compliant, output via logger
-    // ...
-  }
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Application started.");
+    }
+
+    public String getGreeting() {
+        logger.debug("getGreeting() called");
+        return "Hello from App!";
+    }
 }
