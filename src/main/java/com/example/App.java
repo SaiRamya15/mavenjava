c35bdb01e214a2c9619c0e452a8bc84bd75ed440
SaiRamya15 <sairamya5169@gmail.com>
2025-05-17 12:22:03 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,16 +1,16 @@
 /*package com.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+public class App {
+    public String getMessage() {
+        return "Hello, Jenkins!";
+    }
 
-public class AppTest {
-    @Test
-    void testApp() {
-        assertEquals("Hello", "Hello");
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
     }
 }*/
 
-/*import java.util.logging.Logger;
+import java.util.logging.Logger;
 
 class MyClass {
 
@@ -21,17 +21,4 @@ class MyClass {
     logger.info("My Message");  // Compliant, output via logger
     // ...
   }
-}*/
-
-
-package com.example;
-
-public class App {
-    public String getMessage() {
-        return "Hello, Jenkins!";
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
 }
